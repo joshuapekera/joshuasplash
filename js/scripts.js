1b4a0b90e39d54faf971c4c078edae3cfce79535
Joshua Pekera <joshuapekera@me.com>
2012-08-30 23:01:35 -0500

    Update Theme
    Updated a bunch of stuff
    
    Signed-off-by: Joshua Pekera <joshuapekera@me.com>

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Slide Out
+
+if ($(window).width() <= 767) {
+		    $('#navbar').removeClass('bounceInDown').addClass('fadeInDown');
+		    $('#globalnav').removeClass('bounceInLeft').addClass('fadeInLeft');
+		   }
