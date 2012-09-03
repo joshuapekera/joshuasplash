81b498605d5b7ea4502260ee29586631f3f9231e
Joshua Pekera <joshuapekera@me.com>
2012-09-03 02:07:22 -0500

    Update
    major update
    
    Signed-off-by: Joshua Pekera <joshuapekera@me.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,51 @@
-// Slide Out
+// Viewport Slideout
+(function() {
+    
+    var body = document.body,
+            plus = document.querySelectorAll('#page .plus')[0],
+            closeMask = document.getElementById('close-mask'),
+            pageTop = document.getElementById('page'),
+            menuView = document.getElementById('menu');
 
-if ($(window).width() <= 767) {
-		    $('#navbar').removeClass('bounceInDown').addClass('fadeInDown');
-		    $('#globalnav').removeClass('bounceInLeft').addClass('fadeInLeft');
-		   }
+    plus.addEventListener('click', function() {
+        if (hasClass(body, 'menu-open')) {
+            hideMenu();
+        } else {
+            showMenu();
+        }
+    });
+
+    closeMask.addEventListener('click', function(e) {
+        e.preventDefault();
+        hideMenu();
+    });
+
+    setTimeout(function() { window.scrollTo(0, 0); }, 100);
+
+    function showMenu() {
+        // window.scrollTo(0, 0);
+        body.setAttribute('class', 'menu-open');
+        closeMask.style.display = 'block';
+        menuView.style.display = 'block';
+        pageTop.style.width = window.innerWidth - 40 + "px";
+        closeMask.style.width = window.innerWidth + "px";
+        closeMask.style.height = window.outerHeight + "px";
+        menuView.style.height = window.outerHeight + "px";
+    }
+
+    function hideMenu() {
+        window.scrollTo(0, 0);
+        body.removeAttribute('class');
+        closeMask.style.display = 'none';
+        closeMask.removeAttribute('style', 'width');
+        closeMask.removeAttribute('style', 'height');
+        pageTop.removeAttribute('style', 'width');
+        //menuView.style.display = 'none';
+    }
+
+    function hasClass(el, selector) {
+          var className = " " + selector + " ";             
+        return (el.nodeType === 1 && (" " + el.className + " ").replace(/[\n\t\r]/g, " ").indexOf(className) > -1);
+    }
+    
+})();
